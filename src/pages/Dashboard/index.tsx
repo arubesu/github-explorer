21e17708ee3dae154d04cd535e8eb65d06d1345f
Bruno Souza <bruno.arubesu@gmail.com>
2020-10-18 14:52:13 -0300

    feat: styling dashboard search bar, button, repositories items

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,46 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logo from '../../assets/logo.svg';
 
-import { Title } from './styles';
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logo} alt="Github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit"> Pesquisar </button>
+    </Form>
 
-const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
-};
+    <Repositories>
+      <a href="ref">
+        <img src="https://i.stack.imgur.com/frlIf.png" alt="Profile" />
+        <div>
+          <strong>Repository</strong>
+          <p>Description</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="ref">
+        <img src="https://i.stack.imgur.com/frlIf.png" alt="Profile" />
+        <div>
+          <strong>Repository</strong>
+          <p>Description</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+
+      <a href="ref">
+        <img src="https://i.stack.imgur.com/frlIf.png" alt="Profile" />
+        <div>
+          <strong>Repository</strong>
+          <p>Description</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
