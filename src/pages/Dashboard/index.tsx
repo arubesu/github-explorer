97d7c798b394873bf82afdfdedb394ce478ceea9
Bruno Souza <bruno.arubesu@gmail.com>
2020-10-18 16:40:19 -0300

    feat: save repositories in localstorage

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,11 @@
-import React, { FormEvent, useState } from 'react';
+import React, { FormEvent, useState, useEffect } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories, Error } from './styles';
 import api from '../../services/api';
 import logo from '../../assets/logo.svg';
+import Repository from '../Repository';
+
+const localStorageRepositoryKey = '@GithubExplorer:repositories';
 
 interface Repository {
   full_name: string;
@@ -16,7 +19,24 @@ interface Repository {
 const Dashboard: React.FC = () => {
   const [inputError, setInputError] = useState('');
   const [newRepository, setNewRepository] = useState('');
-  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [repositories, setRepositories] = useState<Repository[]>(() => {
+    const storagedRepositories = localStorage.getItem(
+      localStorageRepositoryKey,
+    );
+
+    if (storagedRepositories) {
+      return JSON.parse(storagedRepositories);
+    }
+
+    return [];
+  });
+
+  useEffect(() => {
+    localStorage.setItem(
+      localStorageRepositoryKey,
+      JSON.stringify(repositories),
+    );
+  }, [repositories]);
 
   async function handleAddNewRepository(
     e: FormEvent<HTMLFormElement>,
